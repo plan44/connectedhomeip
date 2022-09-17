2afec6a004a23f300d00d9918b6b48dcd9df13d6
Lukas Zeller <luz@plan44.ch>
2022-09-17 15:50:13 +0200

    [p44 internal use only] Darwin platform manager: need to declare ExitExternalMainLoop() when using with libev

--- src/platform/Darwin/PlatformManagerImpl.h
+++ src/platform/Darwin/PlatformManagerImpl.h
@@ -132,5 +132,13 @@ inline PlatformManagerImpl & PlatformMgrImpl(void)
     return PlatformManagerImpl::sInstance;
 }
 
+#if !CHIP_SYSTEM_CONFIG_USE_DISPATCH
+namespace Internal {
+
+void ExitExternalMainLoop();
+
+} // namespace Internal
+#endif // !CHIP_SYSTEM_CONFIG_USE_DISPATCH
+
 } // namespace DeviceLayer
 } // namespace chip
